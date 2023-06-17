f7f6da46be3d076fc3414db64d98a4010c810935
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-17 22:56:17 +0100

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/*main- entry point but it takes no argument*/
-
+/* main- entry point but it takes no argument */
+/* return- always 0 (sucessed) */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
