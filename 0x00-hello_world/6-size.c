3ed67578f94b66f732c7b947e7154d88bae9f33a
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-18 00:45:05 +0100

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,10 @@
 */
 int main(void)
 {
-printf("size of char: %lu byte(s)\n", sizeof(char));
-printf("size of short: %lu byte(s)\n", sizeof(short));
-printf("size of int: %lu byte(s)\n", sizeof(int));
-printf("size of long: %lu byte(s)\n", sizeof(long));
-printf("size of float: %lu byte(s)\n", sizeof(float));
-printf("size of double: %lu byte(s)\n", sizeof(double));
-printf("size of long double: %lu byte(s)\n", sizeof(long double));
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of a int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
