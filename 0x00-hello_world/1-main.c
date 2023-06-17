a497da8a22c2814d3a474067b2340d2b5acf34aa
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-17 20:55:27 +0100

    compiler

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-/*
-*
-*main-entry point it takes no argument
-*/
-/*return- always 0 (success)*/
 int main(void)
 {
-printf("Holberton School");
-return (0);
+  printf("Holberton School");
 }
