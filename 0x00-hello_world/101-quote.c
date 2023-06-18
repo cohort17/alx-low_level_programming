aac58e811e539c6085b5f7e644768f3dd889d122
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-18 02:51:14 +0100

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main- here is where are code starts,
+*it contains the code we want to execute.
+*
+*return- it shows what the system return after code execution
+*/
+int main()
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
