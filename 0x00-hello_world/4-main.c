60fdfaa5de9135212dbcf3d170db71696696c5af
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-18 01:56:47 +0100

    intel

---
+++ 0x00-hello_world/4-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main- here is where are code starts,
+*it contains the code we want to execute.
+*
+*return- it shows what the system return after code execution
+*/
+int main(void)
+{
+printf("My name is beautiful");
+ return (0);
+}
