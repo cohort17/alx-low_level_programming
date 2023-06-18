15aac109f7a9520735b8045a373d374392384858
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-18 03:29:48 +0100

    updated puts.c comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* main- starting point for code execution */
+/**
+* main- starting point for code execution
+* Return: if its value is 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
