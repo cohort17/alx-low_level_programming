6face117ab93f279fcbf71c7c7fa0d492008222f
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-18 03:11:59 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 *main- here is where are code starts,
 *it contains the code we want to execute.
 *
-*return- it shows what the system return after code execution
+/*return- when it returns 0 the cold is successful.
 */
-int main()
+int main(void)
 {
 fprintf
 (stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
