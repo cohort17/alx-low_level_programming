9652bd56c42ba5e1b806ef6735f18e71539b9a26
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 23:47:32 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main- starting code execution point
+*
+*Return: value after code execution done
+*/
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+putchar('a'+ a);
+}
+putchar('\n');
+return (0);
+}
