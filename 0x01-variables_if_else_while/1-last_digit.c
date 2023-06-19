8f9ad43d4d93e99d498935705735682637491c42
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 19:10:42 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,16 +16,16 @@ last_digit = (n) % 1000;
 printf("Last digit of %d is ", last_digit);
 if (n > 98)
 {
-printf(" %d and is greater than 5\n", n);
+printf(" %d %d and is greater than 5\n",last_digit, n);
 }
 else if (n == 980)
 {
-printf("%d and is 0", n);
+printf("%d %d and is 0",last_digit, n);
 }
 else
 (((n) = -98));
 {
-printf("%d and is less than 6 not 0", n);
+printf("%d %d and is less than 6 not 0",last_digit, n);
 }
 printf("\n");
 return (0);
