bdb4c34619d1e16c0aa6171cb3555d8336efdb67
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 15:05:04 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,18 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = abs(n) % 10;
-printf("Last digit of %d is", last_digit);
-if( last_digit > 5)
+printf( "Last digit of %d is", last_digit);
+if(last_digit > 5)
 {
-printf(" and is greater than 5\n");
+  printf( " %d and is greater than 5\n", last_digit);
 }
-else if( last_digit == 0)
+else if(last_digit == 0)
 {
-printf(" and is 0\n");
+  printf( " %d and is 0\n", last_digit);
 }
 else
 {
-printf(" and is less than 6 not 0\n");
+  printf( " %d and is less than 6 not 0\n", last_digit);
 }
 return (0);
 }
