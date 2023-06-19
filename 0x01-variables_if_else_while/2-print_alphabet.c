09e63b2f3e8673d95a33ad261f7d1850d3ef3147
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 23:21:52 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- starting code execution point
+ *
+ *Return: value after code execution done
+*/
+int main(void)
+{
+int v;
+for (v = 97; v <= 122; v++)
+{
+putchar(v);
+}
+putchar('\n');
+ return (0);
+}
