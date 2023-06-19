7e0c236bccbb4fe00be8e94d8bfcef26770db201
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 09:32:57 +0100

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 printf(" %d", n);
 if (n > 0)
 {
-printf(" is positive");
+printf(" is positive\n");
 }
 else if (n < 0)
 {
-printf(" is negative");
+printf(" is negative\n");
 }
 else
 {
-printf(" is zero");
+printf(" is zero\n");
 }
 return (0);
 }
