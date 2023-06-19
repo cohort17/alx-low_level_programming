4b83d37e12c607d98e8139fc1ccf3db15d51d46f
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-19 17:34:37 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,14 @@ if(last_digit > 5)
 {
 printf(" %d and is greater than 5\n", last_digit);
 }
-else if(((last_digit) == 0))
+else if(last_digit == 0)
 {
-printf(" %d and is 0\n", last_digit);
+printf("%d and is 0", last_digit);
 }
 else
 {
-printf(" %d and is less than 6 and not 0\n", last_digit);
+printf("%d and is less than 6 not 0", last_digit);
 }
+printf("\n");
 return (0);
 }
