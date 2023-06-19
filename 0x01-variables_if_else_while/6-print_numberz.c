068f0963a42905492ffa4a63373e505316712900
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-20 00:15:32 +0100

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main- starting code execution point
+*
+*Return: value after code execution done
+*/
+int main() {
+int i = 0;
+while (1 <= 9)
+{
+putchar('i' + 1);
+++i;
+}
+putchar('\n');
+return 0;
+}
