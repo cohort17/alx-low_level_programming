f05ea949ada98d260cf16d487c8695817e40dc17
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 00:33:13 +0100

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-*
-*print_alphabet_x10- it print all alphabets in lowercase 10 times 
+*print_alphabet_x10- it print all alphabets in lowercase 10 times
 *with a new line
 */
 void print_alphabet_x10(void)
