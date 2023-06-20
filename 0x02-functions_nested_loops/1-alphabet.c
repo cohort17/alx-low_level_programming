0e52b87fa44f686cbfed33675044b6aea7d2a270
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 00:01:59 +0100

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+*
+*print_alphabet- it writes alphabets in lowercase and new lines follows
+*
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*main- it checks the code to be executed
+*
+*Return: it is always return 0
+*/
+int main(void)
+{
+print_alphabet();
+return (0);  
+}
