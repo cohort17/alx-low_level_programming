2abcd3145e9394c6a289d4466db6cb7bc9b0a4c8
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 03:18:49 +0100

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_last_digit - Prints the last digit of a number.
-* @r: The number in question.
+*main- Prints the first 98 Fibonacci numbers, starting with
+*1 and 2, separated by a comma followed by a space.
 *
-* Return: Value of the last digit.
+*Return: Always 0.
 */
-int print_last_digit(int r)
+int main(void)
 {
-int last_digit = r % 10;
-if (last_digit < 0)
-last_digit *= -1;
-_putchar(last_digit + '0');
-return (last_digit);
+int Sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (Sum = 0; Sum < 92; Sum++)
+{
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 = sum;
 }
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+for (Sum = 93; Sum < 99; Sum++)
+{
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (Sum != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
+return (0);
+}
+
