ebac3ebe3d50a01ce951ea8921726bb0b03ec31f
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 02:17:20 +0100

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 /**
-*print_alphabet_x10- it print all alphabets in lowercase 10 times
-*with a new line
+*add - Adds two integers.
+*@num1: it is the first integer to be added.
+*@num2: it is the second integer to be added.
+*
+* Return: The result of the addition.
 */
-void print_alphabet_x10(void)
+int add(int num1, int num2)
 {
-int run = 0;
-char beep;
-while (run++ <= 9)
-{
-for (beep = 'a'; beep <= 'z'; beep++)
-_putchar(beep);
-_putchar('\n');
-}
+return (num1 + num2);
 }
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 /**
 *main- it checks the code to be executed
 *
-*print_alphabet: prints the alphabets
 *Return: it is always return 0
 */
 int main(void)
 {
-print_alphabet_x10();
+int n;
+n = add(89, 9);
+printf("%d\n", n);
 return (0);
 }
