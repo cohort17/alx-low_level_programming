b4583d2fd952fc73a97cb4118e4a73be36dac963
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 01:27:16 +0100

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
 #include "main.h"
 /**
-*print_alphabet_x10- it print all alphabets in lowercase 10 times
-*with a new line
+*_isalpha- it print character if it is alphabet
+*
+*@r: is the character to be checked
 */
-void print_alphabet_x10(void)
+int _isalpha(int r)
 {
-int run = 0;
-char beep;
-while (run++ <= 9)
-{
-for (beep = 'a'; beep <= 'z'; beep++)
-_putchar(beep);
-_putchar('\n');
-}
+if ((r >= 'a' && r <= 'z') ||
+(r >= 'A' && r <= 'Z'))
+return (1);
+else
+return (0);
 }
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -2,11 +2,20 @@
 /**
 *main- it checks the code to be executed
 *
-*print_alphabet: prints the alphabets
 *Return: it is always return 0
 */
 int main(void)
 {
-print_alphabet_x10();
+int r;
+r = _isalpha('H');
+_putchar(r + '0');
+r = _isalpha('o');
+_putchar(r + '0');
+r = _isalpha(108);
+_putchar(r + '0');
+r = _isalpha(';');
+_putchar(r + '0');
+_putchar('\n');
 return (0);
 }
+
