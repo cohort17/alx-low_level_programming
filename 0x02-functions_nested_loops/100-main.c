121317b36830ba6c56fbc9c1a13a98c5e654a955
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-21 02:47:08 +0100

    times tables

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main- it checks the code to be executed
+*
+*Return: it is always return 0
+*/
+int main(void)
+{
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);
+return (0);
+}
