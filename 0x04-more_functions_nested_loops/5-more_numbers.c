932f189d58c3b5553529acd1742ba49137c22fe7
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-22 21:29:13 +0100

    more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*more_numbers - Prints the numbers 0-14 ten times
+*more_numbers- Prints the numbers 0-14 ten times
 *@num: number to be counted
-*@count: number to be counted
+*@count: second number to be counted
 */
 void more_numbers(void)
 {
