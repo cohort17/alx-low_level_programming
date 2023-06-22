37cf9bb08f33b3d8a07649f1a728f72554bb5eea
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-22 22:36:14 +0100

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_most_numbers - Prints the numbers from 0-9 except for 2 and 4
-*@var: it is the number to be checked
+*@num: the number to be printed from
 */
 void print_most_numbers(void)
 {
