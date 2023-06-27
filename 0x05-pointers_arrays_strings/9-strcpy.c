1c35b2d7e3c228482b1dfac367b6376c742d10e4
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-26 22:05:18 -0700

    task completed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
 *
 *Return: A pointer to the destination string @dest.
 */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 int index = 0;
 while (src[index])
@@ -15,5 +15,5 @@ while (src[index])
 dest[index] = src[index];
 index++;
 }
-return (0);
+return (dest);
 }
