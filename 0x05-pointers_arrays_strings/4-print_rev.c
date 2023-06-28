9de86c39ef3066b2eb1af933b5d3f1a6d3757309
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-28 14:36:14 +0100

    task completed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,9 @@
 void print_rev(char *s)
 {
 int len = 0, index;
-while (s[index++])
+while (s[len] != '\0')
 len++;
 for (index = len - 1; index >= 0; index--)
-_putchar(char *c, s[index]);
+_putchar(s[index]);
 _putchar('\n');
 }
