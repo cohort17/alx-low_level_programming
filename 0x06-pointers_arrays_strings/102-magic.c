2076db4920454d913aecb00812fafdedbe140b0f
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-06-30 04:53:12 +0100

    task completed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main(void)
 {
 int n;
@@ -14,7 +15,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
- *(p + 2) = 98;
+ a[2] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
