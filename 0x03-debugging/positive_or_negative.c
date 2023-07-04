6bf6eb3f84055218b77a48c1f99213ea647e83ac
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-03 18:48:46 -0700

    task completed

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,15 @@
 void positive_or_negative(int i)
 {
 if (i > 0)
+{
 printf("%d is positive\n", i);
+}
 else if (i < 0)
+{
 printf("%d is negative\n", i);
+}
 else
-printf("0 is neither positive nor negative\n");
+{
+printf("%d is zero\n", i);
+}
 }
