c47a8cb86c3da69de17389c311b80e5627939d7f
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-03 20:02:20 -0700

    task completed

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,2 +1,27 @@
-1-main    2-largestest_number.c  3-convert_day.c  3-main_b    README.md                 positive_or_negative.c
-1-main.c  2-main                 3-main_a         3-main_b.c  main.c
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
+
+int main(void)
+{
+    int month;
+    int day;
+    int year;
+
+    month = 4;
+    day = 01;
+    year = 1997;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+    day = convert_day(month, day);
+
+    print_remaining_days(month, day, year);
+
+    return (0);
+}
