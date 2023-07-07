c5db3f6c9515b44474810434eed949c4132a169c
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-07 16:54:19 -0700

    task completed

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
-
+/**
+*main- it where code execution starts
+*
+*Return: it gives 0 if execution successful
+*/
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+_puts("\"At the end of the day, my goal was to
+be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
