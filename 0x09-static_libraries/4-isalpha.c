bdffa06c1364893d5f3f060254e36b9e02ebd271
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-07 19:16:19 -0700

    task completed

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,2 +1,16 @@
+#include <stdio.h>
 #include "main.h"
-int _isalpha(int c);
+/**
+*_isalpha- it print character if it is alphabet
+*
+*@n: is the character to be checked
+*Return: it displays 0
+*/
+int _isalpha(int n)
+{
+if ((n >= 'a' && n <= 'z') ||
+(n >= 'A' && n <= 'Z'))
+return (1);
+else
+return (0);
+}
