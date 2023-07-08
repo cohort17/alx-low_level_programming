eabf1482d0a1498928d88ed621cd06c38bfa1904
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-07 20:18:00 -0700

    task completed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,8 +8,8 @@
 */
 int main(int argc, char *argv[])
 {
-  int arg;
-  for (arg = 0; arg < argc; arg++)
+int arg;
+for (arg = 0; arg < argc; arg++)
 printf("%s\n", argv[arg]);
-  return (0);
+return (0);
 }
