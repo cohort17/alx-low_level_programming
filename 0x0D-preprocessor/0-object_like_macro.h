5e547e3fa2371c8e749eb2459077c5436b0ca0b2
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-07-17 15:35:31 -0700

    task completed

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,13 +2,13 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
-*Auth: Eze Chiadikaobi
 *
 *File: header file
+*
 *Description: macro header file that defines
 *a macro named SIZE as an abbreviation for the token 1024
 */
 
 #define SIZE 1024
 
-#endif /* MACRO_H */
+#endif
