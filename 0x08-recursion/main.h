8716e42c80e4286a4c1a1200bce0b37caf75fe50
Eze Chiadikaobi <chiadikaobigideoneze@gmail.com>
2023-10-02 13:02:53 -0700

    task completed

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+**
+*name:Eze Chiadikaobi
+*date:29/sept/2023.
+*file: header file
+*descript: header file for recursion 
+*functions.
+*\
+
+void _puts_recursion(char *s);
+void _puts_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _poe_recursion(iny x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+#endif 
